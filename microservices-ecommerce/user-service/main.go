f5e079d8042b87eedfd01299d56b855ef4ff0dd7
San <99511815+sanowl@users.noreply.github.com>
2024-06-11 13:57:48 +0300

    Update main.go

--- microservices-ecommerce/user-service/main.go
+++ microservices-ecommerce/user-service/main.go
@@ -1,13 +1,61 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type User struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+var users = make(map[string]User)
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(users)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	user, ok := users[params["id"]]
+	if ok {
+		json.NewEncoder(w).Encode(user)
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	users[user.ID] = user
+	json.NewEncoder(w).Encode(user)
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var updatedUser User
+	_ = json.NewDecoder(r.Body).Decode(&updatedUser)
+	users[params["id"]] = updatedUser
+	json.NewEncoder(w).Encode(updatedUser)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	delete(users, params["id"])
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func main() {
-	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "User Service")
-	})
-	http.ListenAndServe(":8081", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/users", getUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", getUser).Methods("GET")
+	r.HandleFunc("/users", createUser).Methods("POST")
+	r.HandleFunc("/users/{id}", updateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+	http.ListenAndServe(":8081", r)
 }
