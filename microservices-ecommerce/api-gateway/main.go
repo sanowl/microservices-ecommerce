b7d4da6e69d2d6cd9398131a1f7150e954ceae99
San <99511815+sanowl@users.noreply.github.com>
2024-06-11 13:56:36 +0300

    Update main.go

--- microservices-ecommerce/api-gateway/main.go
+++ microservices-ecommerce/api-gateway/main.go
@@ -1,13 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func proxyRequest(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	target := vars["service"]
+
+	var url string
+	switch target {
+	case "users":
+		url = "http://user-service:8081" + r.URL.Path
+	case "products":
+		url = "http://product-service:8082" + r.URL.Path
+	case "orders":
+		url = "http://order-service:8083" + r.URL.Path
+	default:
+		http.NotFound(w, r)
+		return
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	w.Write(body)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "API Gateway")
-	})
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/{service}/{rest:.*}", proxyRequest)
+	http.ListenAndServe(":8080", r)
 }
