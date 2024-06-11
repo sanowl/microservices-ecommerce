72740beb67dc438c4d0ae38e57224a0dae2aade0
San <99511815+sanowl@users.noreply.github.com>
2024-06-11 13:57:18 +0300

    Update main.go

--- microservices-ecommerce/product-service/main.go
+++ microservices-ecommerce/product-service/main.go
@@ -1,13 +1,61 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type Product struct {
+	ID    string  `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+var products = make(map[string]Product)
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(products)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	product, ok := products[params["id"]]
+	if ok {
+		json.NewEncoder(w).Encode(product)
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	var product Product
+	_ = json.NewDecoder(r.Body).Decode(&product)
+	products[product.ID] = product
+	json.NewEncoder(w).Encode(product)
+}
+
+func updateProduct(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var updatedProduct Product
+	_ = json.NewDecoder(r.Body).Decode(&updatedProduct)
+	products[params["id"]] = updatedProduct
+	json.NewEncoder(w).Encode(updatedProduct)
+}
+
+func deleteProduct(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	delete(products, params["id"])
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func main() {
-    http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Product Service")
-    })
-    http.ListenAndServe(":8082", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/products", getProducts).Methods("GET")
+	r.HandleFunc("/products/{id}", getProduct).Methods("GET")
+	r.HandleFunc("/products", createProduct).Methods("POST")
+	r.HandleFunc("/products/{id}", updateProduct).Methods("PUT")
+	r.HandleFunc("/products/{id}", deleteProduct).Methods("DELETE")
+	http.ListenAndServe(":8082", r)
 }
